483c92bb0fdd3835d39aa3a368569c6199e25bb4
MartinBallay <ballay.martin307@gmail.com>
2025-03-10 22:56:13 +0100

    uprava hraca

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:305e620d9b417e352fcb8c16fef433f6d42595268d2634f3a37d565473589bfc
+size 550
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d4d1f7aa5bfcd4266d589fc418a690cfdae3d3ca0fadad32649ebd76482afec6
+size 1060
